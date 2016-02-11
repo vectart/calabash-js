1f32889e4b00219049a538fa80b2232f1cd7eaee
Marat Dyatko <marat.dyatko@klarna.com>
2016-02-11 13:52:11 +0100

    Cross-Origin Communication

--- src/calabash.js
+++ src/calabash.js
@@ -17,6 +17,7 @@
         /*TEXT_NODE                      : */ 3 : 'TEXT_NODE',
         /*DOCUMENT_NODE                  : */ 9 : 'DOCUMENT_NODE'
     };
+    var UNESCAPED = String.fromCharCode(37) + '@';
 
     function boundingClientRect(object)
     {
@@ -163,55 +164,91 @@
         return result;
     }
 
-    var exp = '%@'/* dynamic */,
-        queryType = '%@' /* dynamic */,
-        arguments = '%@' /* dynamic */,
-        frameSelector='%@' /* dynamic */,
-        queryFrame= frameSelector == '' ? document : document.querySelectorAll(frameSelector)[0].contentWindow.document,
-        queryWindow = frameSelector == '' ? window : document.querySelectorAll(frameSelector)[0].contentWindow,
-        nodes = null,
-        res = [],
-        i,N;
- 
-    try
-    {
-        if (queryType == 'dump')
+    function fetch(exp, queryType, args, frameSelector) {
+        var queryWindow = frameSelector == '' ? window : document.querySelectorAll(frameSelector)[0].contentWindow,
+            queryFrame = queryWindow.document,
+            nodes = null,
+            res = [],
+            i,N;
+     
+        try
         {
-            return JSON.stringify(dumpTree(queryFrame,
-                                           toJSON(queryFrame,
-                                                  false,
-                                                  queryFrame,
-                                                  queryWindow),
-                                           queryFrame,
-                                           queryWindow));
-        }
-        else if (queryType==='xpath')
-        {
-            nodes=queryFrame.evaluate(exp, queryFrame, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-            for (i=0, N=nodes.snapshotLength; i<N; i++)
+            if (queryType == 'dump')
             {
-                res[i] = nodes.snapshotItem(i);
+                return JSON.stringify(dumpTree(queryFrame,
+                                               toJSON(queryFrame,
+                                                      false,
+                                                      queryFrame,
+                                                      queryWindow),
+                                               queryFrame,
+                                               queryWindow));
+            }
+            else if (queryType==='xpath')
+            {
+                nodes=queryFrame.evaluate(exp, queryFrame, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+                for (i=0, N=nodes.snapshotLength; i<N; i++)
+                {
+                    res[i] = nodes.snapshotItem(i);
+                }
+            } else if (frameSelector != '' && !queryFrame && queryWindow) {
+                var id = window.__CALABASH_RESULTS__.length;
+
+                queryWindow.postMessage({
+                    __CALABASH_REQUEST__: id,
+                    exp: exp,
+                    queryType: queryType,
+                    args: args,
+                    frameSelector: ''
+                }, '*')
+
+                return JSON.stringify({
+                    async: 'window.__CALABASH_RESULTS__['+id+']'
+                })
+            }
+            else
+            {
+                res = queryFrame.querySelectorAll(exp);
             }
         }
-        else
+        catch (e)
         {
-            res = queryFrame.querySelectorAll(exp);
+           return JSON.stringify({error:'Exception while running query: '+exp, details:e.toString()})
         }
-    }
-    catch (e)
-    {
-       return JSON.stringify({error:'Exception while running query: '+exp, details:e.toString()})
-    }
 
 
-    var unescapedString = String.fromCharCode(37); + '@';
-    if (arguments !== unescapedString && arguments !== '') {
-        var length = res.length;
+        if (args !== UNESCAPED && args !== '') {
+            var length = res.length;
 
-        for (var i = 0; i < length; i++) {
-            res[i] = applyMethods(res[i], arguments);
+            for (var i = 0; i < length; i++) {
+                res[i] = applyMethods(res[i], arguments);
+            }
         }
+
+        return JSON.stringify(toJSON(res, true, queryFrame, queryWindow));
     }
 
-    return JSON.stringify(toJSON(res, true, queryFrame, queryWindow));
+    if (!window.__CALABASH_RESULTS__) {
+        window.__CALABASH_RESULTS__ = ['']
+        window.addEventListener('message', function(e){
+            console.warn('MESSAGE', e)
+            if (e.data && e.data.__CALABASH_REQUEST__) {
+                e.source.postMessage({
+                    __CALABASH_RESPONSE__: e.data.__CALABASH_REQUEST__,
+                    result: fetch(e.data.exp, e.data.queryType, e.data.args, e.data.frameSelector)
+                }, '*')
+            }
+            if (e.data && e.data.__CALABASH_RESPONSE__) {
+                window.__CALABASH_RESULTS__[e.data.__CALABASH_RESPONSE__] = e.data.result
+            }
+        }, false);
+    }
+
+    var exp = '%@'/* dynamic */,
+        queryType = '%@' /* dynamic */,
+        args = '%@' /* dynamic */,
+        frameSelector = '%@' /* dynamic */;
+
+    if (exp !== UNESCAPED && exp !== '') {
+        return fetch(exp, queryType, args, frameSelector);
+    }
 })();
